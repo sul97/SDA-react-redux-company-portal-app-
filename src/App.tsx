9523a3b62d3fccabbec1ed2038aad6756fd00ada
thoanguyen2907 <thoanguyen290795@gmail.com>
2023-09-01 13:07:32 +0300

    add toolkit template

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,34 @@
-import { useState } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { Button, Box, Grid, Typography } from '@mui/material'
 
+import { decrement, increment } from './features/counterSlice'
+import { RootState } from './store'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const count = useSelector((state: RootState) => state.counter.value)
+  const dispatch = useDispatch()
 
   return (
     <div className="App">
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
-      </div>
+      <h1>Vite + React + Toolkit + MUI</h1>
+      <Box sx={{ width: '100%' }}>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid item xs={5}>
+            <Button variant="contained" onClick={() => dispatch(increment())}>
+              Increment
+            </Button>
+          </Grid>
+          <Grid item xs={2}>
+            <Typography>{count}</Typography>
+          </Grid>
+          <Grid item xs={5}>
+            <Button variant="contained" onClick={() => dispatch(decrement())}>
+              Decrement
+            </Button>
+          </Grid>
+        </Grid>
+      </Box>
     </div>
   )
 }
